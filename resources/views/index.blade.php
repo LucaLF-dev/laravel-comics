f2290671f43ad4ce75646565cd8681ddf5657b63
LucaLF-dev <laficaraluca@gmail.com>
2023-12-12 19:36:01 +0100

    add : comics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,31 @@
-@extends("layouts.app")
 
-@section("content")
-    
-<section class="py-5">
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+
+  
+  <section class="py-5">
     <div class="container">
-      <h1>pagina Home</h1>
+      <div class="row">
+        @foreach ($comics as $item)
+            <h1>{{$item->price}}</h1>
+        @endforeach
+      </div>
     </div>
   </section>
+
+</body>
+</html>
     
-@endsection
+
+    
+
 
